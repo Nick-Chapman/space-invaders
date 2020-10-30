c7ca599f9041622fc8dd5d7534dc1a5a92ae2273
Nick Chapman <nic@amadido.co.uk>
2020-10-30 02:15:42 +0000

    update expected outputs of tests

--- test.sh
+++ test.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-echo 'running test1...'
-stack run test1 > test1.trace
+
+echo 'running tst...'
+stack run tst > tst.out
 echo 'any diffs?...'
-diff test1.trace.sav test1.trace
+diff tst.expected tst.out
+
+
+echo 'running test1...'
+stack run test1 > test1.out
+echo 'any diffs?...'
+diff test1.expected test1.out
+
 
 echo 'running test2...'
-stack run test2 > test2.trace
+stack run test2 > test2.out
 echo 'any diffs?...'
-diff test2.trace.sav test2.trace
+diff test2.expected test2.out
